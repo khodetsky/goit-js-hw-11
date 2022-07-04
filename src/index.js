e47a270af0187e4db17cd0868e04439a3313826e
khodetsky <hodyaya@gmail.com>
2022-07-05 01:03:01 +0300

    add marcap of gallery

--- src/index.js
+++ src/index.js
@@ -1,38 +1,61 @@
 import './css/styles.css';
+import Notiflix from 'notiflix';
 
-
+import { getUser } from './getUser';
 
 const axios = require('axios').default;
 
 const PIXABEY_KEY = '28393009-563cada9a4af8f72bfd4d9668';
 const BASE_HTTP = 'https://pixabay.com/api/';
 
-const submitBtn = document.querySelector('#submit-button');
+// const submitBtn = document.querySelector('#submit-button');
 const form = document.querySelector('#search-form');
+const gallery = document.querySelector(".gallery");
 
-async function getUser(value) {
-    const searchParams = new URLSearchParams({
-        key: PIXABEY_KEY,
-        q: `${value}`,
-        image_type: "photo",
-        orientation: "horizontal",
-        safesearch: "true",
-        per_page: 40,
-    });
-  try {
-    const response = await axios.get(`${BASE_HTTP}?${searchParams}`);
-    //   const pictures = await response.json();
-        return response;
-  } catch (error) {
-    console.error(error);
-    }
-    
-};
+
 
 function onFormSubmit(evt) {
     evt.preventDefault();
-    // getUser(this.searchQuery.value);
-    getUser(this.searchQuery.value).then(response => console.log(response));;
+
+    getUser(this.searchQuery.value)
+        .then(response => {
+            if (response.data.total === 0) {
+            Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.");
+                return;
+            }
+            // console.log(response.data.hits);
+            return response.data.hits;
+        })
+        .then((response) => {
+            gallery.insertAdjacentHTML("beforeend", cardsMarcup(response));
+            
+        })
+        .catch(() => {});
+};
+
+
+function cardsMarcup(cards) {
+    const marcup = cards.map(card => {
+        return `
+        <li class="photo-card">
+          <img src="${card.webformatURL}" alt="${card.tags}" loading="lazy" />
+          <ul class="info">
+            <li class="info-item">
+              <b>Likes</b> ${card.likes}</p>
+            </li>
+            <li class="info-item">
+              <b>Views</b> <p> ${card.views}</p>
+            </li>
+            <li class="info-item">
+              <b>Comments</b> <p> ${card.comments}</p>
+            </li>
+            <li class="info-item">
+              <b>Downloads</b> <p> ${card.downloads}</p>
+            </li>
+          </ul>
+        </li>`
+    }).join("");
+return marcup;
 };
 
 form.addEventListener('submit', onFormSubmit)
