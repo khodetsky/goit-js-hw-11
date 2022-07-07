65eb41cfadf689742058577315d7426e79fea20c
khodetsky <hodyaya@gmail.com>
2022-07-08 01:49:33 +0300

    update index.js

--- src/index.js
+++ src/index.js
@@ -11,26 +11,34 @@ const BASE_HTTP = 'https://pixabay.com/api/';
 // const submitBtn = document.querySelector('#submit-button');
 const form = document.querySelector('#search-form');
 const gallery = document.querySelector(".gallery");
+const loadMoreBtn = document.querySelector(".load-more");
+document.querySelector(".load-more").hidden = true;
 
-
+let page = '';
 
 function onFormSubmit(evt) {
     evt.preventDefault();
+    page = 1;
+    localStorage.setItem("inputValue", `${this.searchQuery.value}`);
+    document.querySelector(".load-more").hidden = true;
 
-    getUser(this.searchQuery.value)
+    getUser(this.searchQuery.value, page)
         .then(response => {
+            
             if (response.data.total === 0) {
             Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.");
                 return;
             }
-            // console.log(response.data.hits);
+           
             return response.data.hits;
         })
         .then((response) => {
+            gallery.innerHTML = "";
             gallery.insertAdjacentHTML("beforeend", cardsMarcup(response));
+            document.querySelector(".load-more").hidden = false;
             
         })
-        .catch(() => {});
+        .catch(() => { });
 };
 
 
@@ -58,4 +66,38 @@ function cardsMarcup(cards) {
 return marcup;
 };
 
-form.addEventListener('submit', onFormSubmit)
+function endOfContentMcp() {
+    return "<p class ='search-form'>We're sorry, but you've reached the end of search results.</p>"
+}
+
+function onLoadMore() {
+    page += 1;
+    const inputValue = localStorage.getItem("inputValue");
+
+    getUser(inputValue, page)
+        .then(response => {
+            if (page * 40 >= response.data.totalHits) {
+                document.querySelector(".load-more").hidden = true;
+                document.body.insertAdjacentHTML("beforeend", endOfContentMcp());
+            }
+            return response.data.hits;
+        })
+        .then((response) => {
+            gallery.insertAdjacentHTML("beforeend", cardsMarcup(response));
+            
+        }).then(() => {
+            const cardHeight = document
+                .querySelector(".gallery")
+                .firstElementChild.getBoundingClientRect();
+            const cardHeightDoubled = cardHeight.height * 2;
+
+            window.scrollBy({
+              top: `${cardHeightDoubled}`,
+              behavior: "smooth",
+            }); 
+        })
+        .catch(() => { });
+}
+
+form.addEventListener('submit', onFormSubmit);
+loadMoreBtn.addEventListener('click', onLoadMore);
