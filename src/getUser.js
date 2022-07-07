2c68c33103088a86ffe4b5b1545ab7b88e0765ed
khodetsky <hodyaya@gmail.com>
2022-07-08 01:48:53 +0300

    update getUser

--- src/getUser.js
+++ src/getUser.js
@@ -3,17 +3,20 @@ const axios = require('axios').default;
 const PIXABEY_KEY = '28393009-563cada9a4af8f72bfd4d9668';
 const BASE_HTTP = 'https://pixabay.com/api/';
 
-export async function getUser(value) {
-    const searchParams = new URLSearchParams({
+export async function getUser(value, page) {
+
+    const params = {
         key: PIXABEY_KEY,
         q: `${value}`,
         image_type: "photo",
         orientation: "horizontal",
         safesearch: "true",
         per_page: 40,
-    });
+        page: `${page}`,
+    }
+    const searchParams = new URLSearchParams(params);
   try {
-    const response = await axios.get(`${BASE_HTTP}?${searchParams}`);
+      const response = await axios.get(`${BASE_HTTP}?${searchParams}`);
         return response;
   } catch (error) {}   
 };
